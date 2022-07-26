3ad8ba9fadfe9bb37908dca066599af923c02fb2
samsonmeseret <samsonmeseretb@gmail.com>
2022-07-26 13:01:07 -0400

    update

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,5 +1,5 @@
 export const achievementData = {
-  bio: "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+  bio: "I consider every corner of my profession's or Expertise and works hard, studies hard on them ",
   achievements: [
     {
       id: 1,
