dc88f24695d366265e2515b654b3c01fe2326e08
samsonmeseret <samsonmeseretb@gmail.com>
2023-01-13 14:06:24 -0500

    updated the expiriance data

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -13,11 +13,11 @@ export const experienceData = [
     startYear: "2021",
     endYear: "2022",
   },
-  // {
-  //     id: 3,
-  //     company: 'Wonka Industries',
-  //     jobtitle: 'Marketing',
-  //     startYear: '2020',
-  //     endYear: 'Present'
-  // },
+  {
+    id: 3,
+    company: "Evangadi Tech",
+    jobtitle: "Staff",
+    startYear: "2023",
+    endYear: "Present",
+  },
 ];
